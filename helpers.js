f9e443dfb9e1870951f2c53a910c83de772cc84e
Robin Hamill <hi@robinhamill.ca>
2017-10-09 11:48:02 -0400

    Add heck checker helper

--- helpers.js
+++ helpers.js
@@ -4,4 +4,8 @@ module.exports = {
   randomNum(length) {
     return Math.floor(Math.random() * length);
   },
+  isHex(hex) {
+    const a = parseInt(hex,16);
+    return (a.toString(16) === hex.toLowerCase());
+  },
 };
